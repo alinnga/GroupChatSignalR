25aeb7368cdc2e5070b603f19837074727e47cca
alina <alina@DESKTOP-T5VDLC5>
2020-10-08 10:52:30 +0300

    gg

---
+++ GroupChatSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GroupChatSignalR.Models;
+using Microsoft.AspNet.SignalR;
+
+namespace GroupChatSignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        static List<User> Users = new List<User>();
+
+        // Отправка сообщений
+        public void Send(string name, string message, string groupName)
+        {
+            Clients.Group(groupName).addMessage(name, message);
+        }
+
+        public void Join(string group)
+        {
+            Groups.Add(Context.ConnectionId, group);
+        }
+        // Подключение нового пользователя
+        public void Connect(string userName)
+        {
+            var id = Context.ConnectionId;
+
+
+            if (!Users.Any(x => x.ConnectionId == id))
+            {
+                Users.Add(new User { ConnectionId = id, Name = userName });
+
+                // Посылаем сообщение текущему пользователю
+                Clients.Caller.onConnected(id, userName, Users);
+
+                // Посылаем сообщение всем пользователям, кроме текущего
+                Clients.AllExcept(id).onNewUserConnected(id, userName);
+            }
+        }
+
+        // Отключение пользователя
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
+        {
+            var item = Users.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+            if (item != null)
+            {
+                Users.Remove(item);
+                var id = Context.ConnectionId;
+                Clients.All.onUserDisconnected(id, item.Name);
+            }
+
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
